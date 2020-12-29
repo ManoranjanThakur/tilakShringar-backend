70e5fad7f89f541e6c19de92d96c292d8d6f27af
ManoranjanThakur <ManoranjanThakur>
2020-12-30 02:05:01 +0530

    last

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 exports.requireSignin = (req, res, next) => {
     if(req.headers.authorization){
         const token = req.headers.authorization.split(" ")[1];
-        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const user = jwt.verify(token, MERNSECRET);
         req.user = user;
 
     }else{
